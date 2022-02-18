077c45209ad3fc84a41ecb7c05968c3816e67a15
azrahel98 <azrahel9@gmail.com>
2022-02-18 07:43:31 -0500

    testing

--- lib/api/data/login_data.dart
+++ lib/api/data/login_data.dart
@@ -1,3 +1,15 @@
 import 'package:thera/api/repositorios/login.dart';
 
-class LoginData extends LoginReposotory {}
+class LoginData extends LoginReposotory {
+  @override
+  create_user(String email, password, name) {
+    // TODO: implement create_user
+    throw UnimplementedError();
+  }
+
+  @override
+  login(String username, password) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+}
